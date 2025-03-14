1a1aa211db6e1cc653b50fd54384c054c2c3edef
Kim Riyeon <kry59965@gmail.com>
2025-03-14 10:14:45 +0900

    forEach, for-of

--- 문법/for.js
+++ 문법/for.js
@@ -4,9 +4,7 @@ for(let i=0; i<letter.length; i++){
     console.log(letter[i])
 }
 
-letter.forEach(function(l){
-    console.log(l)
-})
+letter.forEach(l=>console.log(l))
 
 for(const l of letter){
     console.log(l)
