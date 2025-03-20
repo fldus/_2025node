306883164126c5b63faa53c83c9ddb12ca9b579a
Kim Riyeon <kry59965@gmail.com>
2025-03-21 08:57:40 +0900

    post 요청

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require('express');
 // Express 애플리케이션을 생성합니다.
 const app = express();
 
-// 루트 경로 ('/')에 GET 요청이 들어오면 실행될 핸들러를 정의합니다.
-app.get('/', (req, res) => {
-  // 클라이언트에게 'Hello, World!'라는 응답을 보냅니다.
-  res.send('Hello, World!\n');
+app.get('/swag', (req, res) => {
+  res.send('get swag');
+});
+
+app.post('/swag', (req, res) => {
+  res.send('post swag');
 });
 
 // 서버가 수신 대기할 포트 번호를 설정합니다.
